eaa183bce18727011f04b377068fe266ee31fd99
newaz1503 <saikotkhan44@gmail.com>
2023-09-07 11:44:16 +0600

    first commit

---
+++ resources/views/admin/layouts/partials/sidebar.blade.php
@@ -0,0 +1,46 @@
+<aside class="left-sidebar">
+    <!-- Sidebar scroll-->
+    <div class="scroll-sidebar">
+        <!-- User Profile-->
+        <div class="user-profile">
+            <div class="user-pro-body">
+                <div><img src="{{asset('assets/admin/')}}/images/users/2.jpg" alt="user-img" class="img-circle"></div>
+                <div class="dropdown">
+                    <a href="javascript:void(0)" class="dropdown-toggle u-dropdown link hide-menu" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">@auth
+                        {{ auth()->user()->name ?? '' }}
+
+                    @endauth <span class="caret"></span></a>
+                    <div class="dropdown-menu animated flipInY">
+                        <!-- text-->
+                        <a href="javascript:void(0)" class="dropdown-item"><i class="ti-user"></i> My Profile</a>
+                        <div class="dropdown-divider"></div>
+                        <!-- text-->
+                        <a href="javascript:void(0)" class="dropdown-item"><i class="ti-settings"></i> Setting</a>
+                        <!-- text-->
+                        <div class="dropdown-divider"></div>
+                        <!-- text-->
+                        <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();" class="dropdown-item"><i class="ti-power-off"></i> Logout</a>
+                        <!-- text-->
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav">
+            <ul id="sidebarnav">
+                <li class="nav-small-cap">--- PERSONAL</li>
+                <li>
+                    <a class="waves-effect waves-dark" href="{{ route('admin.dashboard') }}" aria-expanded="false">
+                        <i class="icon-speedometer"></i>
+                        <span class="hide-menu">Dashboard </span>
+                    </a>
+            </ul>
+        </nav>
+        <!-- End Sidebar navigation -->
+    </div>
+    <!-- End Sidebar scroll-->
+</aside>
